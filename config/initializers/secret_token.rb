6ade5fd07164b500c99ab1ada0980c227f6cf49b
Timothy <barnes7td@gmail.com>
2013-06-16 20:30:04 -0400

    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RudyLynnsQuilts::Application.config.secret_token = '555c8f6e1c486dea110d306f3731ac0527d9c17ad6ea48ead9e1856eb730402da62e3a1490765055363d4a01c39e49bc38df462e7b19ec6a32b605976a1d6233'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RudyLynnsQuilts::Application.config.session_store :cookie_store, key: '_rudy_lynns_quilts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RudyLynnsQuilts::Application.config.session_store :active_record_store
